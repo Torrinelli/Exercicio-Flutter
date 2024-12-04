7895e3c87e402a339f578b719931b5a5be58ac82
Torrinelli <149675103+Torrinelli@users.noreply.github.com>
2024-12-04 15:45:38 -0300

    Add files via upload

---
+++ exercicio_flutter_2.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Botão Centralizado',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              
+              print('Botão pressionado!');
+            },
+            child: const Text('Clique aqui'),
+          ),
+        ),
+      ),
+    );
+  }
+}
