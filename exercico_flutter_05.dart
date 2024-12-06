f3f88bc7b49914c667c296f6bb576fd4269a5f2a
Torrinelli <149675103+Torrinelli@users.noreply.github.com>
2024-12-06 11:25:22 -0300

    Add files via upload

---
+++ exercico_flutter_05.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FittedBox Exemplo',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text('Pequeno', style: TextStyle(fontSize: 24)),
+            ),
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text('Medio', style: TextStyle(fontSize: 36)),
+            ),
+            FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text('Grande', style: TextStyle(fontSize: 48)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
